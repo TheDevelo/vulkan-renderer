3ad36f300f09c805365b0cf3449784ff387a8cd4
Robert Fuchs <robertfuchsyoshi@gmail.com>
2024-04-02 14:08:02 -0400

    Fix analytic Lambertian light values

--- shaders/common.glsl
+++ shaders/common.glsl
@@ -132,14 +132,14 @@ vec4 diffuseLightContribution(LightInfo light, sampler2DShadow shadowMap, vec3 n
         // Sun Light
         // The light direction is (0, 0, -1)
         float cosWeight = max(0.0, dot(lightspaceNormal, vec3(0.0, 0.0, 1.0)));
-        return vec4(light.tint, 1.0) * light.power * cosWeight;
+        return vec4(light.tint, 1.0) * light.power * cosWeight / PI;
     }
     else if (light.type == 1) {
         // Sphere Light
         // The light direction is normalize(lightspacePosition)
         float cosWeight = max(0.0, dot(lightspaceNormal, -normalize(lightspacePosition)));
         float falloff = 1.0 / max(light.radius, dot(lightspacePosition, lightspacePosition));
-        vec4 lightContrib = vec4(light.tint, 1.0) * light.power * falloff * cosWeight / (4 * PI);
+        vec4 lightContrib = vec4(light.tint, 1.0) * light.power * falloff * cosWeight / (4 * PI * PI);
 
         if (light.useLimit) {
             float attenuation = max(0, 1 - pow(length(lightspacePosition) / light.limit, 4.0));
@@ -153,7 +153,7 @@ vec4 diffuseLightContribution(LightInfo light, sampler2DShadow shadowMap, vec3 n
         // We calculate the light the same way as with the sphere light, but then attenuate by how close the light direction is to the spot direction
         float cosWeight = max(0.0, dot(lightspaceNormal, -normalize(lightspacePosition)));
         float falloff = 1.0 / max(light.radius, dot(lightspacePosition, lightspacePosition));
-        vec4 lightContrib = vec4(light.tint, 1.0) * light.power * falloff * cosWeight / (4 * PI);
+        vec4 lightContrib = vec4(light.tint, 1.0) * light.power * falloff * cosWeight / (4 * PI * PI);
 
         // Attenuate by the spot direction
         float halfFov = light.fov / 2.0;
